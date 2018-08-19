505985c6be1214bb6b0c790ab7443671ce140c24
spyton <shiyangfrance@hotmail.com>
2018-08-19 16:37:27 +0200

    TDD jenkins

---
+++ fixture/data/test_runner.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+
+print("unittest command 1")
+print("unittest command 2")
---
+++ script_test.sh
@@ -0,0 +1,2 @@
+pykwalify -d fixture/data/mlag.yaml -s fixture/schema/mlag_schema.yml
+pykwalify -d fixture/data/spine.yaml -s fixture/schema/spine_schema.yml 
